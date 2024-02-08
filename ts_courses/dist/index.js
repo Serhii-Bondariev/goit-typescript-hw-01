4d242e1534f108bcf7edd14f51d8f83d98c8ac9b
Serhii <xx.code4u@gmail.com>
2024-02-08 10:31:41 +0200

    update .map
    Пофіксив права доступу на моєму MacBook

--- ts_courses/dist/index.js
+++ ts_courses/dist/index.js
@@ -1,8 +1,9 @@
-import { concatenation } from "./concatenation";
-const button = document.querySelector("button");
-const input = document.querySelector("input");
+import { concatenation } from './concatenation';
+const button = document.querySelector('button');
+const input = document.querySelector('input');
 if (button && input) {
-  button.addEventListener("click", () => {
-    concatenation(input.value, "hello!");
-  });
+    button.addEventListener('click', () => {
+        concatenation(input.value, 'hello world!!!');
+    });
 }
+//# sourceMappingURL=index.js.map
